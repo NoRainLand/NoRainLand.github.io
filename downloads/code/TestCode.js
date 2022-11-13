d89de79d1aa63745783315c1d754321601569137
NoRainLand <wuxinrufeng@qq.com>
2022-11-13 11:46:41 +0800

    测试代码

---
+++ downloads/code/TestCode.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: NoRain
+ * @Date: 2022-11-13 11:40:29
+ * @Last Modified by: NoRain
+ * @Last Modified time: 2022-11-13 11:42:36
+ */
+class TestCode {
+	index = 0;
+	init() {
+		console.log("hello world");
+	}
+	open() {
+		this.index++;
+		console.log(`open{$0}`, index);
+	}
+	close() {
+        
+    }
+}
