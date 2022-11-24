c6e3fae1203085e575b94e5e9e416710dd035590
NoRainLand <wuxinrufeng@qq.com>
2022-11-24 20:00:32 +0800

    ss

--- js/theme.js
+++ js/theme.js
@@ -52,7 +52,6 @@
 			if (!window.verse) {
 				window.verse = [];
 			}
-
 			if (window.verse.length == 0) {
 				$.get("/text/verse.text", function (data) {
 					var lines = data.split("\n"); //按行读取
@@ -72,47 +71,6 @@
 			$(".verse").text(
 				window.verse[Math.floor(Math.random() * window.verse.length)]
 			);
-		},
-	};
-
-	Theme.showDate = {
-		register: function () {
-			if ($(".about")) {
-				let hour = $(".hour");
-				let tips = $(".tips");
-				let min = $(".min");
-				setInterval(() => {
-					let now = new Date();
-
-					if (now.getSeconds() % 2 == 0) {
-						tips.addClass("tips1");
-						tips.removeClass("tips2");
-					} else {
-						tips.addClass("tips2");
-						tips.removeClass("tips1");
-					}
-
-					let numHour = now.getHours();
-					numHour = numHour < 10 ? numHour + "0" : numHour;
-					let numMin = now.getMinutes();
-					numMin = numMin < 10 ? numMin + "0" : numMin;
-
-					if (numMin == "39") {
-						if(!min.hasClass("min2")){
-							console.log("Miku Miku, you can call me Miku");
-							min.addClass("min2");
-							min.removeClass("min1");
-						}
-					} else {
-						if (min.hasClass("min2")) {
-							min.removeClass("min2");
-							min.addClass("min1");
-						}
-					}
-					hour.text(numHour);
-					min.text(numMin);
-				}, 1000);
-			}
 		},
 	};
 
